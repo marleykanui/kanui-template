3d7ff7804f1807f6019fdb4798f3784712d7bfb0
Marley Kanui <marley.kanui@gmail.com>
2021-09-27 15:18:20 -0500

    kanui template - global commit 14 - updated es-lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,7 @@ module.exports = {
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
